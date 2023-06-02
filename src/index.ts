88868ddcf2f696f361f5942ab844655be368d7f5
Manan Rana <manan_rana@hotmail.com>
2023-06-03 07:45:30 +1000

    loop through multiple tickers & print it just once whilst separating $0 arrays

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ function wait(milliseconds) {
 }
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
   await page.goto(
@@ -25,7 +25,7 @@ function wait(milliseconds) {
   await page.keyboard.type('');
 
   await wait(1000);
-  await page.focus('#password');
+  await page.focus('');
   await page.keyboard.type('');
 
   await wait(1000);
@@ -33,76 +33,101 @@ function wait(milliseconds) {
   await page.waitForSelector(loginButton);
   await page.click(loginButton);
 
-  await wait(1000);
-  await page.goto('https://www2.commsec.com.au/quotes/derivatives?stockCode=BHP&exchangeCode=ASX', {
-    waitUntil: 'networkidle2',
-  });
-
-  // Go to derviatives section
-  // const derivativesLink = await page.$x("//a[contains(., 'Derivatives')]")
-  // await clickByText(page, `Derivatives`);
-  const derivativesLink = 'body > app-root > div > div > sub-nav > nav > ul > li:nth-child(10) > a';
-  // const derivativesLink = 'body > app-root > div > div > sub-nav > nav > ul > li:nth-child(3) > a';  //XJO
-  await page.waitForSelector(derivativesLink);
-  await page.click(derivativesLink);
-
-  // click on the strike filters
-  const strikes =
-    'body > app-root > div > div > div > div.subnav-content__inner.section.section--width-max > derivatives-container > div > div > div > options > div > div.subnav-content-container__head.ng-star-inserted > div.subnav-content-container__filters.ng-star-inserted > filter-drop-down:nth-child(2) > div > div > div.filter-drop-down__selected-wrapper > div.filter-drop-down__button-wrapper > button';
-  await page.waitForSelector(strikes);
-  await page.click(strikes);
-
-  await wait(1000);
-  await page.click('#filter_options_strike3');
-
-  async function checkMultipleExpiry(number: number) {
-    // click on the expiry filters
-    const expiry =
-      'body > app-root > div > div > div > div.subnav-content__inner.section.section--width-max > derivatives-container > div > div > div > options > div > div.subnav-content-container__head.ng-star-inserted > div.subnav-content-container__filters.ng-star-inserted > filter-drop-down-multi-level > div > div > div.filter-drop-down__selected-wrapper > div.filter-drop-down__button-wrapper > button';
-    await page.waitForSelector(expiry);
-    await page.click(expiry);
-
-    await wait(1000);
-    await page.click(
-      'xpath/' +
-        `/html/body/app-root/div/div/div/div[1]/derivatives-container/div/div/div/options/div/div[1]/div[2]/filter-drop-down-multi-level/div/div/div[2]/div/div[1]/ul/li[${number}]`,
-    );
-
-    await wait(1000);
-    const tableSelector =
-      'body > app-root > div > div > div > div.subnav-content__inner.section.section--width-max > derivatives-container > div > div > div > options > div > div.subnav-content-container__body > table > tbody';
-    // const countriesLength = await page.$$eval(`${tableSelector} > tr`, el => el.length);
-    // console.log(countriesLength);
-
-    const tableData = await page.evaluate(() => {
-      const tableRows = Array.from(document.querySelectorAll(`tr`));
-      return tableRows.map(row => {
-        const columns = Array.from(row.querySelectorAll('td'));
-        return columns.map(column => {
-          console.log(column.innerText);
-          return column.innerText;
-        });
-      });
-    });
-
+  async function checkTickers(ticker: string[]) {
     const keepTrackCalls: any = [];
     const keepTrackPuts: any = [];
-    for (let i = 1; i < tableData.length + 1; i++) {
-      const currentRow = tableData[i];
-      if (currentRow !== undefined && currentRow.length > 0) {
-        const checkCallSales = currentRow[4];
-        const checkPutSales = currentRow[11];
-        if (!isNaN(parseInt(checkCallSales))) {
-          if (+currentRow[5] <= 0.5 || +currentRow[5] === 0) {
-            keepTrackCalls.push(currentRow[0]);
-          }
-        }
-        if (!isNaN(parseInt(checkPutSales))) {
-          if (+currentRow[12] <= 0.5 || +currentRow[12] === 0) {
-            keepTrackPuts.push(currentRow[0]);
+    const noPriceCalls: any = [];
+    const noPricePuts: any = [];
+    for (let i = 0; i < ticker.length; i++) {
+      await wait(1000);
+      await page.goto(
+        `https://www2.commsec.com.au/quotes/derivatives?stockCode=${ticker[i]}&exchangeCode=ASX`,
+        {
+          waitUntil: 'networkidle2',
+        },
+      );
+
+      // Go to derviatives section
+      // const derivativesLink = await page.$x("//a[contains(., 'Derivatives')]")
+      // await clickByText(page, `Derivatives`);
+      const derivativesLink =
+        'body > app-root > div > div > sub-nav > nav > ul > li:nth-child(10) > a';
+      // const derivativesLink = 'body > app-root > div > div > sub-nav > nav > ul > li:nth-child(3) > a';  //XJO
+      await page.waitForSelector(derivativesLink);
+      await page.click(derivativesLink);
+
+      // click on the strike filters
+      const strikes =
+        'body > app-root > div > div > div > div.subnav-content__inner.section.section--width-max > derivatives-container > div > div > div > options > div > div.subnav-content-container__head.ng-star-inserted > div.subnav-content-container__filters.ng-star-inserted > filter-drop-down:nth-child(2) > div > div > div.filter-drop-down__selected-wrapper > div.filter-drop-down__button-wrapper > button';
+      await page.waitForSelector(strikes);
+      await page.click(strikes);
+
+      await wait(1000);
+      await page.click('#filter_options_strike3');
+
+      async function checkMultipleExpiry(
+        number: number,
+        callsArray: string[],
+        putsArray: string[],
+      ) {
+        // click on the expiry filters
+        const expiry =
+          'body > app-root > div > div > div > div.subnav-content__inner.section.section--width-max > derivatives-container > div > div > div > options > div > div.subnav-content-container__head.ng-star-inserted > div.subnav-content-container__filters.ng-star-inserted > filter-drop-down-multi-level > div > div > div.filter-drop-down__selected-wrapper > div.filter-drop-down__button-wrapper > button';
+        await page.waitForSelector(expiry);
+        await page.click(expiry);
+
+        await wait(1000);
+        await page.click(
+          'xpath/' +
+            `/html/body/app-root/div/div/div/div[1]/derivatives-container/div/div/div/options/div/div[1]/div[2]/filter-drop-down-multi-level/div/div/div[2]/div/div[1]/ul/li[${number}]`,
+        );
+
+        await wait(1000);
+        const tableSelector =
+          'body > app-root > div > div > div > div.subnav-content__inner.section.section--width-max > derivatives-container > div > div > div > options > div > div.subnav-content-container__body > table > tbody';
+        // const countriesLength = await page.$$eval(`${tableSelector} > tr`, el => el.length);
+        // console.log(countriesLength);
+
+        const tableData = await page.evaluate(() => {
+          const tableRows = Array.from(document.querySelectorAll(`tr`));
+          return tableRows.map(row => {
+            const columns = Array.from(row.querySelectorAll('td'));
+            return columns.map(column => {
+              console.log(column.innerText);
+              return column.innerText;
+            });
+          });
+        });
+
+        for (let i = 0; i < tableData.length; i++) {
+          const currentRow = tableData[i];
+          if (currentRow !== undefined && currentRow.length > 0) {
+            const checkCallSales = currentRow[4];
+            const checkPutSales = currentRow[11];
+            if (!isNaN(parseInt(checkCallSales))) {
+              if (+currentRow[5] <= 0.999 && +currentRow[5] > 0.02) {
+                callsArray.push(currentRow[0]);
+              }
+              if (+currentRow[5] === 0) {
+                noPriceCalls.push(currentRow[0]);
+              }
+            }
+            if (!isNaN(parseInt(checkPutSales))) {
+              if (+currentRow[12] <= 0.999 && +currentRow[12] <= 0.02) {
+                putsArray.push(currentRow[0]);
+              }
+              if (+currentRow[12] === 0) {
+                noPricePuts.push(currentRow[0]);
+              }
+            }
           }
         }
       }
+
+      await checkMultipleExpiry(2, keepTrackCalls, keepTrackPuts);
+      await checkMultipleExpiry(3, keepTrackCalls, keepTrackPuts);
+      await checkMultipleExpiry(4, keepTrackCalls, keepTrackPuts);
+      await checkMultipleExpiry(5, keepTrackCalls, keepTrackPuts);
     }
 
     console.log('====== Calls ========');
@@ -110,12 +135,17 @@ function wait(milliseconds) {
 
     console.log('====== PUTS ========');
     console.log(keepTrackPuts);
+
+    console.log('====== Zero $$ Calls ========');
+    console.log(noPriceCalls);
+
+    console.log('====== Zero $$ PUTS ========');
+    console.log(noPricePuts);
   }
 
-  await checkMultipleExpiry(2);
-  await checkMultipleExpiry(3);
-  await checkMultipleExpiry(4);
-  await checkMultipleExpiry(5);
+  await checkTickers(['BHP', 'WOW', 'CBA']);
+
+  console.log('####### FINISH ##########');
 
   // iterate over tr:nth-child(${i}) on all rows
   // for (let i = 1; i < countriesLength + 1; i++) {
