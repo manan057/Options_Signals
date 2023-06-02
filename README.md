0a3b0a3470f553a1b70d3b558b44dfedeb3848f2
Manan Rana <manan_rana@hotmail.com>
2023-06-02 16:38:11 +1000

    ts projec

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Options_Signals
+
+
+Implement TS - https://www.digitalocean.com/community/tutorials/setting-up-a-node-project-with-typescript#step-2-configuring-the-typescript-compiler
---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  requirePragma: false,
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+};
